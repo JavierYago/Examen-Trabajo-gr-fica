4a915554a6bd0cd3fba57e1706635d1270d6c48f
javier <Javier.Yago@alu.uclm.es>
2023-04-24 16:25:45 +0200

    Se guardá?

--- src/PaqC01/Main.java
+++ src/PaqC01/Main.java
@@ -1,6 +1,7 @@
 package PaqC01;
 
 public class Main {
+    //si pongo esto se guardará?
     public static void main(String[] args) {
         DiseñoContenedores cont = new DiseñoContenedores();
     }
